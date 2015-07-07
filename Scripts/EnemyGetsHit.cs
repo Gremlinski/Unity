65713a55850d39f2eb9e2a00c22547af6b175a20
Gremlinski <gremlinski@googlemail.com>
2015-07-07 20:45:54 +0100

    Scripts

---
+++ Scripts/EnemyGetsHit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGetsHit : MonoBehaviour {
+
+	private float speed = 10.0f;
+	private Vector3 position;
+	private Vector3 previousPosition;
+	private Transform tr;
+
+	private int enemyHP = 100;
+
+	void Start() {
+		position = transform.position;
+		previousPosition = transform.position;
+		tr = transform;
+	}
+	
+	void Update() {
+		transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+		//transform.position = position;
+		previousPosition = position;
+	}
+
+	void TakeDamage(int playerDamage) {
+
+		enemyHP -= playerDamage;
+
+		Debug.Log(gameObject.name + " was hit for " + playerDamage + "hp. " + enemyHP + "left.");
+	}
+}
---
+++ Scripts/MoveInGrid.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInGrid : MonoBehaviour {
+
+	public float speed = 10.0f;
+	private Vector3 position;
+	private Vector3 previousPosition;
+	private Transform tr;
+	private int playerDamage = 10;
+	
+	void Start() {
+		position = transform.position;
+		previousPosition = transform.position;
+		tr = transform;
+	}
+	
+	void Update() {
+
+		if (Input.GetKeyDown(KeyCode.Q) && tr.position == position)
+		{
+			position += Vector3.up + Vector3.left;
+		}
+		else if (Input.GetKeyDown(KeyCode.W) && tr.position == position)
+		{
+			position += Vector3.up;
+		}
+		else if (Input.GetKeyDown(KeyCode.E) && tr.position == position)
+		{
+			position += Vector3.up + Vector3.right;
+		}
+		else if (Input.GetKeyDown(KeyCode.A) && tr.position == position)
+		{
+			position += Vector3.left;
+		}
+		else if (Input.GetKeyDown(KeyCode.D) && tr.position == position)
+		{
+			position += Vector3.right;
+		}
+		else if (Input.GetKeyDown(KeyCode.Z) && tr.position == position)
+		{
+			position += Vector3.down + Vector3.left;
+		}
+		else if (Input.GetKeyDown(KeyCode.X) && tr.position == position)
+		{
+			position += Vector3.down;
+		}
+		else if (Input.GetKeyDown(KeyCode.C) && tr.position == position)
+		{
+			position += Vector3.down + Vector3.right;
+		}
+
+		foreach(GameObject enemyObject in GameObject.FindGameObjectsWithTag("Enemy"))
+		{
+			if(enemyObject.transform.position == position)
+			{
+				enemyObject.SendMessage("TakeDamage", playerDamage);
+
+				//DestroyObject(enemyObject);
+
+				position = previousPosition;
+			}
+		}
+
+
+		transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+		//transform.position = position;
+		previousPosition = position;
+	}  
+
+}
